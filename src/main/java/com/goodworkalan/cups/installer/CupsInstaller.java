85a8e0f2dbf490f2876ef60172386947b977321f
Alan Gutierrez <alan@blogometer.com>
2010-05-04 15:35:32 -0500

    A working Java Cups installer.

--- src/main/java/com/goodworkalan/cups/installer/CupsInstaller.java
+++ src/main/java/com/goodworkalan/cups/installer/CupsInstaller.java
@@ -11,8 +11,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.StringTokenizer;
 
-import com.sun.org.apache.xml.internal.utils.StringToIntTable;
-
 public class CupsInstaller {
     /**
      * Create a file by joinging the string values of the given objects with the
@@ -32,6 +30,28 @@ public class CupsInstaller {
         return new File(file.toString());
     }
 
+    private static boolean fetch(File full, String[] artifact, String suffix) throws IOException {
+        URL url = null;
+        try {
+            url = new URL("http://github.com/downloads/bigeasy/" + artifact[0] + "/" + artifact[1] + "-" + artifact[2] + "." + suffix);
+        } catch (MalformedURLException e) {
+            // Never happens.
+        }
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+            InputStream in = connection.getInputStream();
+            byte[] buffer = new byte[4092];
+            FileOutputStream out = new FileOutputStream(full);
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            out.close();
+            return true;
+        }
+        return false;
+    }
     /**
      * Install Java cups.
      * 
@@ -40,80 +60,87 @@ public class CupsInstaller {
     public static void main(String[] args) throws IOException {
         // Default library is the default Maven repository.
         File library = file(System.getProperty("user.home"), ".m2", "repository").getAbsoluteFile();
+
+        boolean boot = false;
+        boolean download = false;
+        for (String arg :args) {
+            if (arg.equals("--boot")) {
+                boot = true;
+            } else if (arg.equals("--download")) {
+                download = true;
+            } else if (arg.startsWith("--library=")) {
+                library = file(arg.substring(2).split("=", 2)[1]);
+                if (!library.isDirectory()) {
+                    System.err.println("Bad library directory: " + library);
+                }
+            }
+        }
         
-        System.out.println(System.getProperty("os.arch"));
-        System.out.println(System.getProperty("os.name"));
-        System.out.println(System.getProperty("java.ext.dirs"));
-        System.out.println("Hello, World!");
+        if (!(boot || download)) {
+            boot = download = true;
+        }
         
         String[][] artifacts = new String[][] {
+                new String[] { "go-go", "go-go-boot", "0.1.2.5" },
                 new String[] { "retry", "retry", "0.1" },
                 new String[] { "verbiage", "verbiage", "0.1" },
                 new String[] { "danger", "danger", "0.1" },
+                new String[] { "github4j", "github4j-downloads", "0.1" },
                 new String[] { "class-boxer", "class-boxer", "0.1" },
                 new String[] { "class-association", "class-association", "0.1" },
                 new String[] { "infuse", "infuse", "0.1" },
-                new String[] { "go-go", "go-go", "0.1.4" },
-                new String[] { "go-go", "go-go-boot", "0.1.2" },
+                new String[] { "go-go", "go-go", "0.1.4.5" },
+                new String[] { "comfort-io", "comfort-io", "0.1.1" },
                 new String[] { "infuse", "infuse", "0.1" },
                 new String[] { "reflective", "reflective", "0.1" },
                 new String[] { "madlib", "madlib", "0.1" },
-                new String[] { "cups", "cups", "0.1" }
+                new String[] { "cups", "cups", "0.1.1.5" }
         };
-        HttpURLConnection.setFollowRedirects(true);
-        for (String[] artifact :artifacts) {
-            File full = file(library, "com", "github", "bigeasy", artifact[0], artifact[1], artifact[2], artifact[1] + "-" + artifact[2] + ".jar");
-            File directory = full.getParentFile();
-            if (!(directory.isDirectory() || directory.mkdirs())) {
-                throw new IOException("Cannot create directory: " + directory);
-            }
-            URL url = null;
-            try {
-                url = new URL("http://github.com/downloads/bigeasy/" + artifact[0] + "/" + artifact[1] + "-" + artifact[2] + ".jar");
-            } catch (MalformedURLException e) {
-                // Never happens.
-            }
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                InputStream in = connection.getInputStream();
-                byte[] buffer = new byte[4092];
-                FileOutputStream out = new FileOutputStream(full);
-                int read;
-                while ((read = in.read(buffer)) != -1) {
-                    out.write(buffer, 0, read);
+        if (download) {
+            HttpURLConnection.setFollowRedirects(true);
+            for (String[] artifact :artifacts) {
+                File directory = file(library, "com", "github", "bigeasy", artifact[0], artifact[1], artifact[2]);
+                if (!(directory.isDirectory() || directory.mkdirs())) {
+                    throw new IOException("Cannot create directory: " + directory);
                 }
-                in.close();
-                out.close();
-            } else {
-                System.err.println("Unable to install "+ artifact[1] + "-" + artifact[2] + ".jar.");
-                System.exit(1);
+                for (String suffix : new String[] { "dep", "jar" }) {
+                    File full = file(directory, artifact[1] + "-" + artifact[2] + "." + suffix);
+                    if (!fetch(full, artifact, suffix)) {
+                        System.err.println("Unable to install "+ artifact[1] + "-" + artifact[2] + "." + suffix + ".");
+                        System.exit(1);
+                    }
+                    System.out.println("Installed " + artifact[1] + "-" + artifact[2] + "." + suffix + ".");
+                }
             }
-            System.out.println("Installed " + artifact[1] + "-" + artifact[2] + ".jar.");
         }
-        StringTokenizer tokenizer = new StringTokenizer(System.getProperty("java.ext.dirs"), File.pathSeparator);
-        boolean written = false;
-        while (tokenizer.hasMoreElements()) {
-            File dir = file(tokenizer.nextToken());
-            if (dir.canWrite()) {
-                for (File file : dir.listFiles()) {
-                    if (file.getName().startsWith("go-go-boot")) {
-                        System.out.println("Deleting old " + file.getAbsolutePath() + ".");
-                        file.delete();
+        if (boot) {
+            String[] gogoBoot = artifacts[0];
+            StringTokenizer tokenizer = new StringTokenizer(System.getProperty("java.ext.dirs"), File.pathSeparator);
+            boolean written = false;
+            while (tokenizer.hasMoreElements()) {
+                File dir = file(tokenizer.nextToken());
+                if (dir.canWrite()) {
+                    for (File file : dir.listFiles()) {
+                        if (file.getName().startsWith("go-go-boot")) {
+                            System.out.println("Deleting old " + file.getAbsolutePath() + ".");
+                            file.delete();
+                        }
                     }
                 }
-            }
-            if (!written && dir.canWrite()) {
-                System.out.println("Installing " + file(dir, "go-go-boot-0.1.2.jar").getAbsolutePath() + ".");
-                written = true;
-                InputStream in = new FileInputStream(file(library, "com", "github", "bigeasy", "go-go", "go-go-boot", "0.1.2", "go-go-boot-0.1.2.jar"));
-                OutputStream out = new FileOutputStream(file(dir, "go-go-boot-0.1.2.jar"));
-                byte[] buffer = new byte[4092];
-                int read;
-                while ((read = in.read(buffer)) != -1) {
-                    out.write(buffer, 0, read);
+                if (!written && dir.canWrite()) {
+                    System.out.println("Installing " + file(dir, "go-go-boot-" + gogoBoot[2] + ".jar").getAbsolutePath() + ".");
+                    written = true;
+                    InputStream in = new FileInputStream(file(library, "com", "github", "bigeasy", "go-go", "go-go-boot", gogoBoot[2], "go-go-boot-" + gogoBoot[2] + ".jar"));
+    
+                    OutputStream out = new FileOutputStream(file(dir, "go-go-boot-" + gogoBoot[2] + ".jar"));
+                    byte[] buffer = new byte[4092];
+                    int read;
+                    while ((read = in.read(buffer)) != -1) {
+                        out.write(buffer, 0, read);
+                    }
+                    in.close();
+                    out.close();
                 }
-                in.close();
-                out.close();
             }
         }
     }

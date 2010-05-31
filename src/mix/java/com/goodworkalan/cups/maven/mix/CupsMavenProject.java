467b6a51d56139de6619992d864af89875d8d8e3
Alan Gutierrez <alan@blogometer.com>
2010-05-31 10:36:37 -0500

    Updated Mix build.

--- src/mix/java/com/goodworkalan/cups/maven/mix/CupsMavenProject.java
+++ src/mix/java/com/goodworkalan/cups/maven/mix/CupsMavenProject.java
@@ -4,22 +4,27 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class CupsMavenProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Cups Maven.
+ *
+ * @author Alan Gutierrez
+ */
+public class CupsMavenProject implements ProjectModule {
+    /**
+     * Build the project definition for Cups Maven.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces("com.github.bigeasy.cups/cups-maven/0.1.1.3")
-                .main()
-                    .depends()
-                        .include("com.github.bigeasy.cups/cups/0.1.+0")
-                        .include("com.github.bigeasy.comfort-xml/comfort-xml/0.1.+0")
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .end()
+                .produces("com.github.bigeasy.cups/cups-maven/0.1.1.4")
+                .depends()
+                    .production("com.github.bigeasy.cups/cups/0.1.+0")
+                    .production("com.github.bigeasy.madlib/madlib/0.+1")
+                    .production("com.github.bigeasy.comfort-xml/comfort-xml/0.1.+0")
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
                 .end()
             .end();

1c84601e69c3dc838c4bddae2931a8d0d5fd2952
Alan Gutierrez <alan@blogometer.com>
2010-05-31 18:32:48 -0500

    Deleted MavenResolver, fixed Mix build.:

--- src/mix/java/com/goodworkalan/cups/mix/CupsProject.java
+++ src/mix/java/com/goodworkalan/cups/mix/CupsProject.java
@@ -4,8 +4,18 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class CupsProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Cups.
+ *
+ * @author Alan Gutierrez
+ */
+public class CupsProject implements ProjectModule {
+    /**
+     * Build the project definition for Cups.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
@@ -15,7 +25,6 @@ public class CupsProject extends ProjectModule {
                         .include("com.github.bigeasy.github4j/github4j-downloads/0.+1")
                         .include("com.github.bigeasy.go-go/go-go/0.+1")
                         .include("com.github.bigeasy.comfort-io/comfort-io/0.+1")
-                        .include("com.github.bigeasy.madlib/madlib/0.+1")
                         .include("com.github.bigeasy.danger/danger/0.+1")
                         .end()
                     .end()

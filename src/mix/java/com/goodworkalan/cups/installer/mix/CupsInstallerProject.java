2fdb7c623fde437694f4b8214b5875e32ae1a0f3
Alan Gutierrez <alan@blogometer.com>
2010-06-02 20:05:19 -0500

    Dependencies read from Mix task generated file.

--- src/mix/java/com/goodworkalan/cups/installer/mix/CupsInstallerProject.java
+++ src/mix/java/com/goodworkalan/cups/installer/mix/CupsInstallerProject.java
@@ -1,20 +1,63 @@
 package com.goodworkalan.danger.mix;
 
+import com.goodworkalan.go.go.Environment;
+import com.goodworkalan.go.go.library.Include;
+import com.goodworkalan.go.go.library.ArtifactPart;
+import com.goodworkalan.go.go.library.ResolutionPart;
+import com.goodworkalan.go.go.library.PathPart;
+import com.goodworkalan.mix.Mix;
+import com.goodworkalan.mix.Project;
 import com.goodworkalan.mix.ProjectModule;
+import com.goodworkalan.mix.builder.Executable;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
+import com.goodworkalan.comfort.io.Files;
+import java.util.Collections;
+import java.util.TreeSet;
+import java.util.Set;
+import java.io.File;
 
-public class CupsInstallerProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Cups Installer.
+ *
+ * @author Alan Gutierrez
+ */
+public class CupsInstallerProject implements ProjectModule {
+    /**
+     * Build the project definition for Cups Installer.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces("com.github.bigeasy.cups/cups-installer/0.1.1.21")
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .end()
+                .produces("com.github.bigeasy.cups/cups-installer/0.1.1.28")
+                .depends()
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
+                .end()
+            .end();
+        builder
+            .recipe("dependencies")
+                .executable(new Executable() {
+                    public void execute(Environment env) {
+                        Set<String> artifacts = new TreeSet<String>();
+                        for (String pattern : new String[] { 
+                            "com.github.bigeasy.go-go/go-go-boot/+0",
+                            "com.github.bigeasy.cups/cups/+0"
+                        }) {
+                            ArtifactPart artifactPart = env.library.getArtifactPart(new Include(pattern), "dep", "jar");
+                            for (PathPart part : env.library.resolve(Collections.<PathPart>singleton(new ResolutionPart(artifactPart.getArtifact())))) {
+                                artifacts.add(part.getArtifact().toString()); 
+                            }
+                        }
+                        File dependencies = new File("src/main/resources/com/goodworkalan/cups/installer/dependencies.txt");
+                        dependencies.getParentFile().mkdirs();
+                        Files.pour(dependencies, artifacts);
+                        env.io.out.println(artifacts);
+                    }
+                })
                 .end()
             .end();
     }

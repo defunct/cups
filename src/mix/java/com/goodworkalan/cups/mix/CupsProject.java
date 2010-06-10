6b9f1ca78f016fdce77008f094303eca6f480677
Alan Gutierrez <alan@blogometer.com>
2010-06-09 20:01:28 -0500

    Update Mix build after refactor of Mix.

--- src/mix/java/com/goodworkalan/cups/mix/CupsProject.java
+++ src/mix/java/com/goodworkalan/cups/mix/CupsProject.java
@@ -2,7 +2,7 @@ package com.goodworkalan.mix.cups;
 
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
-import com.goodworkalan.mix.builder.JavaProject;
+import com.goodworkalan.mix.cookbook.JavaProject;
 
 /**
  * Builds the project definition for Cups.
@@ -20,18 +20,12 @@ public class CupsProject implements ProjectModule {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.cups/cups/0.1.1.11")
-                .main()
-                    .depends()
-                        .include("com.github.bigeasy.github4j/github4j-downloads/0.+1")
-                        .include("com.github.bigeasy.go-go/go-go/0.+1")
-                        .include("com.github.bigeasy.comfort-io/comfort-io/0.+1")
-                        .include("com.github.bigeasy.danger/danger/0.+1")
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .end()
+                .depends()
+                    .production("com.github.bigeasy.github4j/github4j-downloads/0.+1")
+                    .production("com.github.bigeasy.go-go/go-go/0.+1")
+                    .production("com.github.bigeasy.comfort-io/comfort-io/0.+1")
+                    .production("com.github.bigeasy.danger/danger/0.+1")
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
                 .end()
             .end();

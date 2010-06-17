626293639de5f85e144af722e844eb84d93bfa97
Alan Gutierrez <alan@blogometer.com>
2010-06-17 13:51:17 -0500

    Refactored for upstream changes to Verbiage.

--- src/main/java/com/goodworkalan/cups/CupsError.java
+++ src/main/java/com/goodworkalan/cups/CupsError.java
@@ -1,15 +1,9 @@
 package com.goodworkalan.cups;
 
-import java.util.ResourceBundle;
-import java.util.concurrent.ConcurrentHashMap;
-
 import com.goodworkalan.danger.ContextualDanger;
 
 @SuppressWarnings("serial")
 public class CupsError extends ContextualDanger {
-    /** A cache of resource bundles. */
-    private final static ConcurrentHashMap<String, ResourceBundle> bundles = new ConcurrentHashMap<String, ResourceBundle>();
-
     /**
      * Create a mix error with the given error code.
      * 
@@ -19,7 +13,7 @@ public class CupsError extends ContextualDanger {
      *            The error code.
      */
     public CupsError(Class<?> context, String code, Object...arguments) {
-        super(bundles, context, code, null, arguments);
+        super(context, code, null, arguments);
     }
 
     /**
@@ -33,6 +27,6 @@ public class CupsError extends ContextualDanger {
      *            The cause.
      */
     public CupsError(Class<?> context, String code, Throwable cause, Object...arguments) {
-        super(bundles, context, code, cause);
+        super(context, code, cause);
     }
 }
